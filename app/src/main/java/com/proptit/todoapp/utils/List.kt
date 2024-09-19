5be966163aab240391cbc7c2af0f2c16990cac02
TrDucChinh <chinhdevptit@gmail.com>
2024-09-19 22:12:01 +0700

    UI: Chose Priority

--- app/src/main/java/com/proptit/todoapp/utils/List.kt
+++ app/src/main/java/com/proptit/todoapp/utils/List.kt
@@ -2,11 +2,24 @@ package com.proptit.todoapp.utils
 
 import androidx.fragment.app.Fragment
 import com.proptit.todoapp.ui.HomeFragment
+import com.proptit.todoapp.item.ItemPriority
 
 
 object List{
     val fragmentList = arrayListOf<Fragment>(
         HomeFragment()
+    )
+    val priorityItems = listOf(
+        ItemPriority(1),
+        ItemPriority(2),
+        ItemPriority(3),
+        ItemPriority(4),
+        ItemPriority(5),
+        ItemPriority(6),
+        ItemPriority(7),
+        ItemPriority(8),
+        ItemPriority(9),
+        ItemPriority(10),
     )
 
 }
