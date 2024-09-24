1ab4ecb86d54af0b5a8f0862173a1bf3d773b2b5
TrDucChinh <chinhdevptit@gmail.com>
2024-09-24 16:59:13 +0700

    DB for category

--- app/src/main/java/com/proptit/todoapp/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/proptit/todoapp/viewmodel/CategoryViewModel.kt
@@ -1,17 +1,26 @@
 package com.proptit.todoapp.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.proptit.todoapp.database.TodoDatabase
 import com.proptit.todoapp.database.category.CategoryRepository
+import com.proptit.todoapp.model.Category
+import com.proptit.todoapp.utils.List
 
-class CategoryViewModel() : ViewModel() {
+class CategoryViewModel(application: Application) : ViewModel() {
+    private val categoryRepository: CategoryRepository = CategoryRepository(application, List.categoryItems)
 
+    fun getAllCategory() = categoryRepository.getAllCategory()
+    suspend fun insertCategory(category: Category) = categoryRepository.insertCategory(category)
+    suspend fun deleteCategory(category: Category) = categoryRepository.deleteCategory(category)
+    suspend fun updateCategory(category: Category) = categoryRepository.updateCategory(category)
 
-    class CategoryViewModelFactory(private val repository: CategoryRepository) : ViewModelProvider.Factory{
+    class CategoryViewModelFactory(private val application: Application) : ViewModelProvider.Factory{
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(CategoryViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return CategoryViewModel() as T
+                return CategoryViewModel(application) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
