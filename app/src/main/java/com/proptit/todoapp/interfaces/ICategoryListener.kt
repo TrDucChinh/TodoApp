10c792d81e4607cb6b8ea420605e0b124a517f3d
Trần Đức Chính <140167017+TrDucChinh@users.noreply.github.com>
2024-10-04 21:39:58 +0700

    Revert "Update Task"

--- app/src/main/java/com/proptit/todoapp/interfaces/ICategoryListener.kt
+++
@@ -1,8 +0,0 @@
-package com.proptit.todoapp.interfaces
-
-import com.proptit.todoapp.model.Category
-
-interface ICategoryListener {
-    fun onCategoryClick(category: Category)
-    fun onAddCategoryClick()
-}
